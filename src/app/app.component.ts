28e77ab9681e33ff3e397f16f81f86ba9b927784
Sumit Kumar <j4sumit@gmail.com>
2024-03-01 00:48:41 +0530

    Style Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular 14';
-users =["Sumit","amit","Sanjay","Rahual","Pramod"]
-userDetails =[
-  {name:"Sumit", Email:"Sumit@gmail.com", SocialAccount:["LinkedIN","Wapp","FB"]},
-  {name:"Amit", Email:"aumit@gmail.com", SocialAccount:["FB","LinkedIN","Wapp"]},
-  {name:"Sanjay", Email:"Sanjay@gmail.com", SocialAccount:["Wapp","FB","LinkedIN"]},
-  {name:"Rahul", Email:"Rahul@gmail.com", SocialAccount:["YouTube","FB","LinkedIN"]}
-]
+color="red"
+bgColor="green"
+
+changeColor(){
+  this.color="black"
+  this.bgColor="yellow"
+}
 }
