09d646e6a18ce92a0a5b30162b8e241a3b262839
Sumit Kumar <j4sumit@gmail.com>
2024-03-01 00:29:04 +0530

    Nested Loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,9 @@ export class AppComponent {
   title = 'Angular 14';
 users =["Sumit","amit","Sanjay","Rahual","Pramod"]
 userDetails =[
-  {name:"Sumit", Email:"Sumit@gmail.com", phone:"908904380911"},
-  {name:"Amit", Email:"aumit@gmail.com", phone:"908904380922"},
-  {name:"Sanjay", Email:"Sanjay@gmail.com", phone:"908904380933"},
-  {name:"Rahul", Email:"Rahul@gmail.com", phone:"908904380944"}
+  {name:"Sumit", Email:"Sumit@gmail.com", SocialAccount:["LinkedIN","Wapp","FB"]},
+  {name:"Amit", Email:"aumit@gmail.com", SocialAccount:["FB","LinkedIN","Wapp"]},
+  {name:"Sanjay", Email:"Sanjay@gmail.com", SocialAccount:["Wapp","FB","LinkedIN"]},
+  {name:"Rahul", Email:"Rahul@gmail.com", SocialAccount:["YouTube","FB","LinkedIN"]}
 ]
 }
