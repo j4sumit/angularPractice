32cd74b19dba8e6d7abe1a43701dbdc6b65deb78
Sumit Kumar <j4sumit@gmail.com>
2024-03-01 01:10:19 +0530

    Header Created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular 14';
-color="red"
-bgColor="green"
-
-changeColor(){
-  this.color="black"
-  this.bgColor="yellow"
-}
 }
