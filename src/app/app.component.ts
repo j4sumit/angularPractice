ea81b2619d7a9f89149fc758e30f5fc86ca1fdc8
Sumit Kumar <j4sumit@gmail.com>
2024-02-29 12:06:47 +0530

    CLI command practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = ':- Angular 14';
+  title = 'Angular 14';
+  
+  getVal(){
+    return "Result from function"
+  }
 }
