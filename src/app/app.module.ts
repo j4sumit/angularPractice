27e8b0456e128de7f30d02c12741f756a2523d45
Sumit Kumar <j4sumit@gmail.com>
2024-02-29 12:58:02 +0530

    Module in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { User2ListComponent } from './user2-list/user2-list.component';
-import { User3ListComponent } from './user3-list/user3-list.component';
+import { UserAuthModule } from './user-auth/user-auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent,
-    User2ListComponent,
-    User3ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
