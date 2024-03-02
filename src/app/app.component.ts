17edfba88200953e479f86c5f61a05bb97d9a930
Sumit Kumar <j4sumit@gmail.com>
2024-03-02 22:59:32 +0530

    Reactive form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core'
-import {FormControl, FormGroup} from '@angular/forms'
+import {FormControl, FormGroup,Validators} from '@angular/forms'
 
 
 
@@ -11,12 +11,23 @@ import {FormControl, FormGroup} from '@angular/forms'
 export class AppComponent {
   title = 'Angular 14';
  loginForm= new FormGroup({
-  name:new FormControl(""),
-  email:new FormControl(""),
-  password: new FormControl("")
+  name:new FormControl("",[Validators.required,Validators.pattern('[a-zA-z]+$')]),
+  email:new FormControl("",[Validators.required, Validators.email]),
+  password: new FormControl("",[Validators.required, Validators.minLength(5)])
  })
  loginUser()
  {
   console.log(this.loginForm.value)
  }
+
+ get name(){
+  return this.loginForm.get("name")
+ }
+ get email(){
+  return this.loginForm.get("email")
+ }
+ get password(){
+  return this.loginForm.get("password")
+ }
+
 }
