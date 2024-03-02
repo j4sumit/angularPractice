3e6b50cec17735c94e8c658001e6528df89ebe67
Sumit Kumar <j4sumit@gmail.com>
2024-03-02 16:17:54 +0530

    Child to parent data transfer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,10 @@ import { Component } from '@angular/core'
 })
 export class AppComponent {
   title = 'Angular 14';
-  userDataisl=[
-    {name:"Anil",email:"anil1@gmail.com"},
-    {name:"Anil1",email:"anil2@gmail.com"},
-    {name:"Anil2",email:"anil3@gmail.com"},
-    {name:"Anil3",email:"anil4@gmail.com"}
-  ]
+  item="X"
+
+  updateData(data:string){
+console.log(data);
+this.item=data;
+  }
 }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -7,11 +7,7 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
   constructor() { }
-  @Input() data:{name:string, email:string}={
-    name: '',
-    email: ''
-  };
-
+  @Output() updateEvent = new EventEmitter<string>()
   ngOnInit(): void {
   }
 
