1fdcba5fdc4453db0e9dc25dfc8de4600fac4c61
Sumit Kumar <j4sumit@gmail.com>
2024-03-02 13:15:24 +0530

    ToggleElement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,8 @@ import {NgForm} from '@angular/forms'
 })
 export class AppComponent {
   title = 'Angular 14';
-  userData:any;
-getData(data:NgForm){
-  console.log(data)
-  this.userData=data;
+  show=true 
+toggle(){
+  this.show=!this.show;
 }
 }
