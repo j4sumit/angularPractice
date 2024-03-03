edfc5fba5d48c617d676018f84a959cb34a3f2c5
Sumit Kumar <j4sumit@gmail.com>
2024-03-03 11:10:20 +0530

    Directives in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import {MatButtonModule} from '@angular/material/button'
 import {MatBadgeModule} from '@angular/material/badge';
-import {ReactiveFormsModule} from '@angular/forms'
+import {ReactiveFormsModule} from '@angular/forms';
+import { RedColorDirective } from './red-color.directive'
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    RedColorDirective,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/red-color.directive.ts
@@ -0,0 +1,12 @@
+import { Directive,ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appRedColor]'
+})
+export class RedColorDirective {
+
+  constructor(el: ElementRef) { 
+    el.nativeElement.style.color="red"
+  }
+
+}
