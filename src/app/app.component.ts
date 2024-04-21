92915066133f09fec271923d98355b30c74f1875
Sumit Kumar <j4sumit@gmail.com>
2024-04-21 16:53:36 +0530

    Parent to child data transfer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,9 @@ import { Component } from '@angular/core'
 })
 export class AppComponent {
   title = 'Angular 14';
-  userDataisl=[
-    {name:"Anil",email:"anil1@gmail.com"},
-    {name:"Anil1",email:"anil2@gmail.com"},
-    {name:"Anil2",email:"anil3@gmail.com"},
-    {name:"Anil3",email:"anil4@gmail.com"}
-  ]
+  data=10;
+
+  updateData(){
+    this.data= Math.floor(Math.random()*10)
+  }
 }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -7,10 +7,11 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
   constructor() { }
-  @Input() data:{name:string, email:string}={
-    name: '',
-    email: ''
-  };
+  // @Input() data:{name:string, email:string}={
+  //   name: '',
+  //   email: ''
+  // };
+  @ Input() item:any;
 
   ngOnInit(): void {
   }
